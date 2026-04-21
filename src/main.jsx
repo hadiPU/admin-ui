9aeb5c447c6479c08cdc89355e0cb5e05e552b78
hadiPU <hadiprio575@gmail.com>
2026-04-21 08:21:43 +0700

    membuat User Card

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDom from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Exercise from './Exercise.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDom.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Exercise />
+  </React.StrictMode>
+);
