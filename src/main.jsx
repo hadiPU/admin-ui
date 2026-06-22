eb27a385679052fd1eb68371fd0e16a689279e8b
hadiPU <hadiprio575@gmail.com>
2026-06-22 11:34:19 +0700

    Frontend-Backend integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { ThemeContextProvider } from './context/ThemeContext.jsx';
-createRoot(document.getElementById('root')).render(
+//import App from './App.jsx'
+//import { ThemeContextProvider } from './context/ThemeContext.jsx';
+import Form from "./latihan/Form.jsx";
+
+/*createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeContextProvider>
       <App />
     </ThemeContextProvider>
+  </StrictMode>,
+) */
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+      <Form />
   </StrictMode>,
 ) 
